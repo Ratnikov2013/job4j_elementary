4e746c7a965369a93606ebae6a3e355001a6ca18
Lenovo <ratnikov_iv@mail.ru>
2020-03-25 14:57:01 +0100

    Task

---
+++ src/Taskvariable.java
@@ -0,0 +1,20 @@
+public class Taskvariable {
+    public static void main (String[]args){
+        byte age = 101;
+        char sex = 'M';
+        int heght = 175;
+        int brick =5000;
+        int bacterium = 2000000;
+        int distance = 384483;
+        boolean job = true;
+        short file = 128;
+
+
+
+
+
+
+
+
+    }
+}
