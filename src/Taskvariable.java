430be02015f4e8576eb6d68d289d1f3523d17be4
Lenovo <ratnikov_iv@mail.ru>
2020-03-28 12:01:51 +0100

    FitWheigtmanwoman

--- src/Taskvariable.java
+++ src/Taskvariable.java
@@ -3,7 +3,7 @@ public class Taskvariable {
         byte age = 101;
         char sex = 'M';
         int heght = 175;
-        int brick =5000;
+        int brick = 5000;
         int bacterium = 2000000;
         int distance = 384483;
         boolean job = true;
---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(double height) {
+        double r = (height - 100) * 1.15;
+        return r;
+
+    }
+    public static double womanWeight(double height) {
+        double r = (height - 110) * 1.15;
+        return r;
+    }
+    public static void main(String[]args) {
+        double woman = Fit.womanWeight(172);
+        double man = Fit.manWeight(174);
+
+        System.out.println(woman);
+
+
+
+
+    }
+
+
+}
